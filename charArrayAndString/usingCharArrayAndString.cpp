25dcaf80b23f3a6922d06e9b851fddf06431eea7
Muhammed Seyrek <91323889+muhammedSeyrek@users.noreply.github.com>
2022-08-28 09:45:15 +0300

    Add files via upload

---
+++ charArrayAndString/usingCharArrayAndString.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstring>
+using namespace std;
+int main(){
+	//definitions
+	
+	//define a single character
+	char a;
+	
+	//define a single char array or string
+	
+	char b[20];
+	
+	char c[] = "Sakarya";
+	
+	char *d = "Sakarya";
+	
+	char e[] = {'S', 'a', 'k', 'a', 'r', 'y', 'a'};
+	
+	//define a string array
+	
+	char f[] [10] = {"Bolu", "Sakarya", "Izmit", "Yalova"};
+	char *g[4] = {"Bolu", "Sakarya", "Izmit", "Yalova" };
+	string h[] = {"Bolu", "Sakarya", "Izmit", "Yalova"};	
+	
+	return 0;
+}
---
+++ charArrayAndString/usingCharArrayAndString.java
@@ -0,0 +1,29 @@
+package springIntro;
+
+import java.util.*;
+
+public class usingCharArrayAndString {
+
+	public static void main(String[] args) {
+		//definitions
+		
+		//define a single character
+		char a;
+		
+		//define a single char array or string
+		
+		char b[] = new char[20];
+		
+		String c = "Sakarya";
+		
+		String d = new String("Sakarya");
+		
+		char e[] = {'S', 'a', 'k', 'a', 'r', 'y', 'a'};
+		
+		String f[] = {"Bolu", "Sakarya", "Izmit", "Yalova"};
+				
+	}
+
+}
+
+
